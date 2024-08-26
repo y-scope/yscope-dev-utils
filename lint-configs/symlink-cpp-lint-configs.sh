65a78d62fc1c2a00a0405c4bf1d2639d1034dda7
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-08-26 16:02:03 -0400

    Remove unnecessary function keyword.

--- lint-configs/symlink-cpp-lint-configs.sh
+++ lint-configs/symlink-cpp-lint-configs.sh
@@ -11,7 +11,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # Symlinks the given config file to the repo's root.
 #
 # @param $1 Path to the config file in the repo.
-function symlink_config () {
+symlink_config () {
     config_file_path="$1"
 
     repo_dir="$(git rev-parse --show-toplevel)"
