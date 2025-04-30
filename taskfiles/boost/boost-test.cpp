dabe6b2f85fd9997eab531b0e640b6acab48d804
sitaowang1998 <sitaowang1998@outlook.com>
2025-04-30 01:09:19 +0000

    Add simple tests for each library

--- taskfiles/boost/boost-test.cpp
+++ taskfiles/boost/boost-test.cpp
@@ -1,12 +1,25 @@
+#include <chrono>
+#include <future>
 #include <iostream>
 #include <string>
+#include <string_view>
+#include <thread>
 
 #include <boost/any/bad_any_cast.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/use_future.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/iostreams/device/back_inserter.hpp>
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/process/v2/process.hpp>
 #include <boost/program_options/errors.hpp>
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/parsers.hpp>
 #include <boost/program_options/value_semantic.hpp>
 #include <boost/program_options/variables_map.hpp>
+#include <boost/regex.hpp>
+#include <boost/url/parse.hpp>
+#include <boost/url/url_view.hpp>
 
 namespace {
 auto parse_args(int argc, char** argv) -> boost::program_options::variables_map {
@@ -24,8 +37,60 @@ auto parse_args(int argc, char** argv) -> boost::program_options::variables_map 
     return variables;
 }
 
+auto test_filesystem() -> bool {
+    boost::filesystem::path const path = boost::filesystem::path(__FILE__);
+    return path.has_parent_path();
+}
+
+auto test_iostreams() -> bool {
+    std::string result;
+    boost::iostreams::filtering_ostream out{boost::iostreams::back_inserter(result)};
+    out << "Hello World!";
+    out.flush();
+    return result == "Hello World!";
+}
+
+constexpr int cWaitTime = 10;
+
+auto test_process() -> bool {
+    boost::asio::io_context io_context;
+    boost::process::v2::process process{io_context, "/bin/true", {}};
+    std::future<int> result = process.async_wait(boost::asio::use_future);
+    io_context.run_for(std::chrono::milliseconds(cWaitTime));
+    if (std::future_status::ready != result.wait_for(std::chrono::milliseconds(cWaitTime))) {
+        return false;
+    }
+    return 0 == result.get();
+}
+
+constexpr std::string_view cRegex = "(\\d{4}[- ]){3}\\d{4}";
+constexpr std::string_view cMatch = "1234-5678 9012-3456";
+
+auto test_regex() -> bool {
+    boost::regex const e{std::string{cRegex}};
+    return boost::regex_match(std::string{cMatch}, e);
+}
+
+constexpr std::string_view cUrl
+        = "https://user:pass@example.com:443/path/to/"
+          "my%2dfile.txt?id=42&name=John%20Doe+Jingleheimer%2DSchmidt#page%20anchor";
+
+auto test_url() -> bool {
+    boost::system::result<boost::urls::url_view> result = boost::urls::parse_uri(cUrl);
+    if (result.has_error()) {
+        return false;
+    }
+    boost::urls::url_view url_view = result.value();
+    return "example.com" == url_view.encoded_host_address();
+}
+
 constexpr int cCmdArgParseErr = 1;
 constexpr int cSizeErr = 2;
+constexpr int cFileSystemErr = 3;
+constexpr int cIoStreamsErr = 4;
+constexpr int cProcessErr = 5;
+constexpr int cRegexErr = 6;
+constexpr int cUrlErr = 7;
 }  // namespace
 
 auto main(int argc, char** argv) -> int {
@@ -56,6 +121,26 @@ auto main(int argc, char** argv) -> int {
     if (size != input.size()) {
         std::cerr << "Error: Size mismatch. Expected " << input.size() << ", got " << size << ".\n";
         return cSizeErr;
+    }
+
+    if (false == test_filesystem()) {
+        return cFileSystemErr;
+    }
+
+    if (false == test_iostreams()) {
+        return cIoStreamsErr;
+    }
+
+    if (false == test_process()) {
+        return cProcessErr;
+    }
+
+    if (false == test_regex()) {
+        return cRegexErr;
+    }
+
+    if (false == test_url()) {
+        return cUrlErr;
     }
 
     return 0;
